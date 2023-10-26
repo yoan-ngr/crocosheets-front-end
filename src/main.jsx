9f742a65ca1fc1a7d97ec2e97200c651471313ad
yoan-ngr <yoan.nougueruiz@gmail.com>
2023-10-26 13:07:30 +0200

    Début front-end, ajout routes login register

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+    createBrowserRouter, Outlet,
+    RouterProvider,
+} from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div><Navbar />Hello world!<Outlet/></div>,
+        children : [
+            {
+                path: "/",
+                element: <div>Accueil</div>,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            }
+        ]
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
