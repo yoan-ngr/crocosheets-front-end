774b3370a7aa29f36496d716dcb6d46c613f4030
MatthieuGalante <matthieu.galante@gmail.com>
2023-10-26 14:50:04 +0200

    LE LIEN VERS LE REGISTER ATTENTION CA ARRIVE

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,56 +5,89 @@ function Register () {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSignIn = () => {
+    const handleSignUp = () => {
         console.log('Email:', email);
         console.log('Password:', password);
     };
 
+    const handleMailType = (e) => {
+        setEmail(e.target.value)
+    };
+
     return <div className="mx-auto flex w-full max-w-sm flex-col gap-6">
         <div className="flex flex-col items-center">
-            <h1 className="text-3xl font-semibold">Sign In</h1>
-            <p className="text-sm">Sign in to access your account</p>
+            <h1 className="text-3xl font-semibold">Inscription</h1>
+            <p className="text-sm">Creez un compte pour accéder à CrocoSHEET</p>
         </div>
         <div className="form-group">
             <div className="form-field">
-                <label className="form-label">Email address</label>
+                <label className="form-label">Adresse Email</label>
 
                 <input
-                    placeholder="Type here"
+                    placeholder="adresse mail"
+                    type="email"
+                    className="input max-w-full"
+                    value={email}
+                    onChange={handleMailType}
+                />                <label className="form-label">
+                <span className="form-label-alt">Please enter a valid email.</span>
+            </label>
+            </div>
+
+            <div className="form-field">
+                <label className="form-label">Prénom</label>
+
+                <input
+                    placeholder="prenom"
                     type="email"
                     className="input max-w-full"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />                <label className="form-label">
-                <span className="form-label-alt">Please enter a valid email.</span>
             </label>
             </div>
+
             <div className="form-field">
-                <label className="form-label">Password</label>
+                <label className="form-label">Nom</label>
+
+                <input
+                    placeholder="nom"
+                    type="email"
+                    className="input max-w-full"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />                <label className="form-label">
+            </label>
+            </div>
+
+            <div className="form-field">
+                <label className="form-label">Mot de passe</label>
                 <div className="form-control">
                     <input
-                        placeholder="Type here"
+                        placeholder="mot de passe"
+                        type="password"
+                        className="input max-w-full"
+                        value={password}
+                        onChange={handleMailType}
+                    />                </div>
+            </div>
+
+            <div className="form-field">
+                <label className="form-label">Confirmation du mot de passe</label>
+                <div className="form-control">
+                    <input
+                        placeholder="confirmation mot de passe"
                         type="password"
                         className="input max-w-full"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />                </div>
             </div>
-            <div className="form-field">
-                <div className="form-control justify-between">
-                    <div className="flex gap-2">
-                        <input type="checkbox" className="checkbox" />
-                        <a href="#">Remember me</a>
-                    </div>
-                    <label className="form-label">
-                        <a className="link link-underline-hover link-primary text-sm">Forgot your password?</a>
-                    </label>
-                </div>
-            </div>
+
             <div className="form-field pt-5">
                 <div className="form-control justify-between">
-                    <button type="button" className="btn btn-primary w-full" onClick={handleSignIn}>
-                        Sign in
+                    <button type="button" className="btn btn-success w-full" onClick={handleSignUp}>
+                        Sign Up
                     </button>
                 </div>
             </div>
