ef45efdf83a7ce1a2078e38b55a54669e55605b1
Julien Feraux <56791765+julienferaux@users.noreply.github.com>
2023-11-21 14:10:09 +0100

    fonction de mise a jour d'une case pour le serveur

--- src/pages/Sheet.jsx
+++ src/pages/Sheet.jsx
@@ -3,11 +3,14 @@ import React, { useState, useEffect } from 'react';
 function Sheet() {
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const [selectedCell, setSelectedCell] = useState(null);
+    const [cellValues, setCellValues] = useState(Array.from({ length: 26 }, () => Array.from({ length: 26 }, () => '')));
     let cell_focus;
 
 
     const handleCellClick = (rowIndex, colIndex) => {
+        if(selectedCell != null && cell_focus != null) enregistrer_case(selectedCell.row,selectedCell.col)
         changer_de_case(rowIndex,colIndex)
+
     };
 
     const handleCellDoubleClick = (e) => {
@@ -15,60 +18,64 @@ function Sheet() {
         e.target.focus();
         cell_focus = e.target;
     };
+    function enregistrer_case(rowIndex,colIndex){ // fonction appelé apres qu'une case a été modifier
+        const updatedCellValues = [...cellValues];
+        updatedCellValues[rowIndex][colIndex] = document.getElementById(`cell-${rowIndex}-${colIndex}`).textContent.replace(/\n/g, '');
+        setCellValues(updatedCellValues);
 
-    const handleCellKeyDown = (e) => {
+        serveur_modifier_case(rowIndex,colIndex)
+    }
+    function serveur_modifier_case(rowIndex,colIndex){
+        console.log("le serveur doit metre a jour la case ("+rowIndex+" ; "+colIndex +") avec la valeur : "+ cellValues[rowIndex][colIndex])
+    }
+    function changer_de_case(rowIndex,colIndex) {
+        if (rowIndex >= 0 && rowIndex <= 25 && colIndex >= 0 && colIndex <= 25) {
+            setSelectedCell({row: rowIndex, col: colIndex});
+        }
+    }
+
+    const handleCellKeyDown = (e, rowIndex, colIndex) => {
         if (e.key === 'Enter') {
+            enregistrer_case(rowIndex, colIndex);
             e.preventDefault();
             e.target.contentEditable = false;
             e.target.blur();
-            e.target.innerText = e.target.innerText.replace(/\n/g, '');
         }
     };
-
-    function changer_de_case(rowIndex,colIndex){
-        console.log(selectedCell)
-        if(selectedCell != null) console.log(`${selectedCell.row} - ${selectedCell.col}`);
-        if(rowIndex>=0 && rowIndex<=25 && colIndex>=0 && colIndex<=25){
-            setSelectedCell({ row: rowIndex, col: colIndex });
-        }
-    }
-    // Gestion de l'événement global pour l'appui sur "Entrée"
-
-    const handleGlobalKeyDown = (e) => {
-        if (e.key === 'Enter' && selectedCell ) {
-            const selectedCellElement = document.querySelector(`#cell-${selectedCell.row}-${selectedCell.col}`);
-
-            if (selectedCellElement && (cell_focus !== selectedCellElement)) {
-                e.preventDefault(); // Empêche l'ajout de la nouvelle ligne
-                selectedCellElement.contentEditable = true;
-                selectedCellElement.focus();
-                cell_focus = selectedCellElement;
-            } else if (selectedCellElement && cell_focus === selectedCellElement) {
-                selectedCellElement.blur(); // Supprime le focus de la cellule en cours d'édition
-                cell_focus = null;
-            }
-
-        }else if ((['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) && cell_focus !== document.activeElement) {
-            e.preventDefault(); // Empêche le défilement de la page avec les touches de direction
-
-            if (e.key === 'ArrowUp') {
-                changer_de_case(selectedCell.row - 1,selectedCell.col )
-            } else if (e.key === 'ArrowDown' ) {
-                changer_de_case(selectedCell.row + 1,selectedCell.col )
-            } else if (e.key === 'ArrowLeft') {
-                changer_de_case(selectedCell.row ,selectedCell.col -1 )
-            } else if (e.key === 'ArrowRight') {
-                changer_de_case(selectedCell.row ,selectedCell.col +1 )
-            }
-        }
-    };
-
 
     useEffect(() => {
-        // Écoute des touches enfoncées au niveau de la page entière
+        const handleGlobalKeyDown = (e) => {
+            if (e.key === 'Enter' && selectedCell ) {
+                const selectedCellElement = document.querySelector(`#cell-${selectedCell.row}-${selectedCell.col}`);
+
+                if (selectedCellElement && (cell_focus !== selectedCellElement)) {
+                    e.preventDefault(); // Empêche l'ajout de la nouvelle ligne
+                    selectedCellElement.contentEditable = true;
+                    selectedCellElement.focus();
+                    cell_focus = selectedCellElement;
+                } else if (selectedCellElement && cell_focus === selectedCellElement) {
+                    selectedCellElement.blur(); // Supprime le focus de la cellule en cours d'édition
+                    cell_focus = null;
+                }
+
+            }else if ((['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) && cell_focus !== document.activeElement) {
+                e.preventDefault(); // Empêche le défilement de la page avec les touches de direction
+
+                if (e.key === 'ArrowUp') {
+                    changer_de_case(selectedCell.row - 1,selectedCell.col )
+                } else if (e.key === 'ArrowDown' ) {
+                    changer_de_case(selectedCell.row + 1,selectedCell.col )
+                } else if (e.key === 'ArrowLeft') {
+                    changer_de_case(selectedCell.row ,selectedCell.col -1 )
+                } else if (e.key === 'ArrowRight') {
+                    changer_de_case(selectedCell.row ,selectedCell.col +1 )
+                }
+            }
+        };
+
+
         window.addEventListener('keydown', handleGlobalKeyDown);
         return () => {
-            // Nettoyage de l'écouteur d'événement lors du démontage du composant
             window.removeEventListener('keydown', handleGlobalKeyDown);
         };
     }, [selectedCell]);
@@ -92,7 +99,7 @@ function Sheet() {
                         <td className="w-12">{alphabet.charAt(rowIndex)}</td>
                         {Array.from({ length: 26 }, (_, colIndex) => (
                             <td
-                                key={`cell-${rowIndex}-${colIndex}`} // Add a unique key here
+                                key={`cell-${rowIndex}-${colIndex}`}
                                 id={`cell-${rowIndex}-${colIndex}`}
                                 contentEditable={selectedCell && selectedCell.row === rowIndex && selectedCell.col === colIndex}
                                 className={`w-48 max-w-[6rem] overflow-x-hidden border-solid border border-black font-mono p-2 ${
@@ -100,8 +107,9 @@ function Sheet() {
                                 }`}
                                 onClick={() => handleCellClick(rowIndex, colIndex)}
                                 onDoubleClick={handleCellDoubleClick}
-                                onKeyDown={handleCellKeyDown}
-                            ></td>
+                                onKeyDown={(e) => handleCellKeyDown(e, rowIndex, colIndex)}
+                                dangerouslySetInnerHTML={{ __html: cellValues[rowIndex][colIndex] }}
+                            />
                         ))}
                     </tr>
                 ))}
