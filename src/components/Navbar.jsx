6361b73b2364893fa42c9104f8a40d341052ac40
Yoan Nougué-Ruiz <yoan.nougueruiz@gmail.com>
2023-11-30 13:32:35 +0100

    Amélioration affichage navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
-import {useState} from "react";
-import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Link, redirect} from "react-router-dom";
 import Logo from "../assets/crocosheets.png";
 import {useCookies} from "react-cookie";
+import axios from "axios";
 
 function Navbar () {
 
     const [loggedIn, setLoggedIn] = useState(true);
 
     const [cookies, setCookies] = useCookies(["user"])
+
+    const handleLogOut = () => {
+        setCookies("user", null);
+        // TODO requête déconnexion
+    }
 
     return <div className="navbar rounded-lg">
         <div className="navbar-start">
@@ -19,12 +25,12 @@ function Navbar () {
                     <div className="dropdown-container">
                         <div className="dropdown">
                             <label className="btn btn-ghost flex cursor-pointer px-0" tabIndex="0">
-                                <img src="https://i.pravatar.cc/150?u=a042581f4e29026024d" alt="avatar"/>
+                                <div>{cookies.user.first_name.charAt(0).toUpperCase() + cookies.user.last_name.charAt(0).toUpperCase()}</div>
                             </label>
                             <div className="dropdown-menu dropdown-menu-bottom-left">
                                 <Link to='/dashboard' className="dropdown-item text-sm">Mes documents</Link>
                                 <Link to='/profile' tabIndex="-1" className="dropdown-item text-sm">Mon profil</Link>
-                                <a tabIndex="-1" className="dropdown-item text-sm">Déconnexion</a>
+                                <a tabIndex="-1" className="dropdown-item text-sm" onClick={handleLogOut}>Déconnexion</a>
                             </div>
                         </div>
                     </div>
