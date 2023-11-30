61b676e288f0d09b61ea6aa7dc9d49f3e651d08d
HugoBien <hugo.bienvenot9@etu.univ-lorraine.fr>
2023-11-30 15:06:33 +0100

    Début page de profil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,12 +2,24 @@ import PageTitle from "../components/PageTitle.jsx";
 import Trashbin from "../components/icons/Trashbin.jsx";
 import {Link} from "react-router-dom";
 import SheetTableElement from "../components/SheetTableElement.jsx";
+import {useCookies} from "react-cookie";
 import React from "react";
 
 function Profile () {
+    const [cookies, setCookies] = useCookies(["user"])
     return <div >
 
-        <h1 className="text-4xl font-semibold">Page de profil</h1>
+        <PageTitle text="Profil" />
+        <div className="mx-16 text-center">
+            <p className="text-xl" >Vos informations</p>
+            <div className="mx-[36rem] my-12" >
+                <p className="text-xl text-left my-5" >Nom</p>
+                <p className="text-xl text-left my-5" >Prénom</p>
+                <p className="text-xl text-left my-5" >Email</p>
+                <p className="text-xl text-left my-5" >Mot de passe</p>
+            </div>
+        </div>
+
     </div>
 }
 
