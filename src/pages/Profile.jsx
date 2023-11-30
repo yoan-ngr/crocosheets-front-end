a9285b012e7ad830e730485565d5b9f363c11ccf
HugoBien <hugo.bienvenot9@etu.univ-lorraine.fr>
2023-11-30 14:28:50 +0100

    Ajout route vers la page de Profile.jsx

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import PageTitle from "../components/PageTitle.jsx";
+import Trashbin from "../components/icons/Trashbin.jsx";
+import {Link} from "react-router-dom";
+import SheetTableElement from "../components/SheetTableElement.jsx";
+import React from "react";
+
+function Profile () {
+    return <div >
+
+        <h1 className="text-4xl font-semibold">Page de profil</h1>
+    </div>
+}
+
+export default Profile;
